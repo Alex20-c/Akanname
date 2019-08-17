b46b7a7fff5bfb83bd910d9f342924c3154e497c
Alex Kiprop <kipropalex59@gmail.com>
2019-08-17 19:05:59 +0300

    add the validation

--- javascript/style.js
+++ javascript/style.js
@@ -6,7 +6,7 @@ function getinput(){
   dayOfMonth=parseInt(document.getElementById("monthday").value);
 
 
-  if(century==""){
+  if(century == ""){
     alert("input the correct gender");
     return false;
   }else if(year ==""){
@@ -20,10 +20,47 @@ function getinput(){
     return false;
   }
 }
-//calculate func
+//Calculate func
 function calculateDay(){
   getinput();
-  dayOfWeek = ((((century/4)-2*century-1)+((5*year/4) )+((26*(month+1)/10))+dayOfMonth) %7)-1;
-  console.log(dayOfWeek);//Test the calculateDay function
-  return(Math.floor(dayOfWeek));
+  dayOfWeek = ((((century/4)-2*century-1) + ((5*year/4) + ((26*(month+1)/10)) +
+dayOfWeek)%7)-1;
+console.log(dayOfWeek);//Test the calculateDay function
+return(Math.floor(dayOfWeek));
+if(dayOfWeek<0){
+  dayOfWeek=dayOfWeek*-1;
+  }
+  else if(dayOfWeek>0){
+    return dayOfWeek;
+  }
+}
+
+//main caller func
+function checkDayOfWeek(){
+  day=calculateDay();
+  checkGender();
+  console.log("The function runs");//Test checkDayOfWeek function
+}
+
+//arrays
+let daysOfWeek=["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
+let makeNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+
+
+//get selected radio button
+function checkGender(){
+  var gen=document.getElementsByName("rads");
+  if(gen[0].checked==true){
+    var gender="male";
+  }else if(gen[1].checked==true){
+    var gender="female";
+  }else{
+    console.log("pass");//Test the radio buttons
+  }
+  switch(gender){
+    case gender="male":
+      switch(day){
+        
+      }
+  }
 }
