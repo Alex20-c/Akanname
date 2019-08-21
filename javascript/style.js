0e830438eb5a715e690534ff85d14802cff52738
Alex Kiprop <kipropalex59@gmail.com>
2019-08-21 17:12:31 +0300

    changed the id of the gender and that of the day so that when entering the details it will work well

--- javascript/style.js
+++ javascript/style.js
@@ -1,117 +1,271 @@
-var century, year, month, dayOfMonth, day;
-//Get input
-function getinput() {
-  century = parseInt(document.getElementById("century").value);
-  year = parseInt(document.getElementById("year").value);
-  month = parseInt(document.getElementById("month").value);
-  dayOfMonth = parseInt(document.getElementById("day").value);
+var maleNames = {
+
+  Sunday: 'Kwasi',
+
+Monday: 'Kwadwo',
+
+Tuesday: 'Kwabena',
+
+Wednesday: 'Kwaku',
+
+Thursday: 'Yaw',
+
+Friday: 'Kofi',
+
+Saturday: 'Kwame',
+
+};
+
+var femaleNames = {
+
+Sunday: 'Akosua',
+
+Monday: 'Adwoa',
+
+Tuesday: 'Abenaa',
+
+Wednesday: 'Akua',
+
+Thursday: 'Yaa',
+
+Friday: 'Afua',
+
+Saturday: 'Ama',
+
+};
 
 
-  if (century == "") {
-    alert("input the correct gender");
-    return false;
-  } else if (year == "") {
-    alert("input the correct year");
-    return false;
-  } else if (month == "") {
-    alert("input the correct month");
-    return false;
-  } else if (dayOfMonth == "") {
-    alert("input the correct date");
-    return false;
-  }
+
+function validation() {
+
+var date = document.getElementById("d").value;
+
+
+
+var month = document.getElementById("month").value;
+
+var year = document.getElementById("year").value;
+
+var gen = document.getElementsByName("gd");
+
+var currentDate = new Date();
+
+var currentYear = currentDate.getFullYear();
+
+if(isNaN(date) || date === null || date === "" || date < 1 || date > 31){
+
+ 
+
+  document.getElementById('one').innerHTML = "invalid day";
+
+  document.getElementById('one').style.color = "red"
+
+  alert("invalid date");
+
+  return false;
+
 }
-//Calculate func
-function calculateDay() {
-  getinput();
-  dayOfWeek = ((((century / 4) - 2 * century - 1) + ((5 * year / 4) + ((26 * (month + 1) / 10)) +
-    dayOfMonth) % 7) - 1);
-  console.log(dayOfWeek);//Test the calculateDay function
-  return (Math.floor(dayOfWeek));
-  if (dayOfWeek < 0) {
-    dayOfWeek = dayOfWeek * -1;
-  }
-  else if (dayOfWeek > 0) {
-    return dayOfWeek;
-  }
+
+var ListofDays = [31,28,31,30,31,30,31,31,30,31,30,31];
+
+if(isNaN(month) || month=== "" || month ===null || month <1 || month>12){
+
+  document.getElementById('two').innerHTML = "invalid month";
+
+  document.getElementById('two').style.color = "red"
+
+  alert("invalid month");
+
+ return false;
+
 }
 
-//main caller func
-function checkDayOfWeek() {
-  day = calculateDay();
-  checkGender();
-  console.log("The function runs");//Test checkDayOfWeek function
+if(month===1 || month>2){
+
+if (date>ListofDays[month-1])
+
+{
+
+alert('Invalid date format!');
+
+return false;
+
 }
 
-//arrays
-let daysOfWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-let makeNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+}
+
+if(month===2){
+
+var lyear = false;
+
+if ( (!(year % 4) && year % 100) || !(year % 400)) 
+
+{
 
 
-//get selected radio button
-function checkGender() {
-  var gen = document.getElementsByName("rads");
-  if (gen[0].checked == true) {
-    var gender = "male";
-  } else if (gen[1].checked == true) {
-    var gender = "female";
-  } else {
-    console.log("pass");//Test the radio buttons
-  }
-  switch (gender) {
-    case gender = "male":
-      switch (day) {
-        case (0 || -0):
-          document.getElementById("result").innerHTML = "The Day is on a sunday." + "" + "Your akan name is" + maleNames[0];
-          break;
-        case (1 || -1):
-          document.getElementById("result").innerHTML = "The day is on a monday." + "" + "Your akan name is" + maleNames[1];
-          break;
-        case (2 || -2):
-          document.getElementById("result").innerHTML = "The day is on tuesday." + "" + "Your akan name is" + maleNames[2];
-          break;
-        case (3 || -3):
-          document.getElementById("result").innerHTML = "The day is on a wednesday." + "" + "Your akan name is" + maleNames[3];
-          break;
-        case (4 || -4):
-          document.getElementById("result").innerHTML = "The day is on a thursday." + "" + "Your akan name is" + maleNames[4]
-        case (5 || -5):
-          document.getElementById("result").innerHTML = "The day is on a friday." + "" + "Your akan name is" + maleNames[5]
-          break;
-        case (6 || -6):
-          document.getElementById("result").innerHTML = "The day is on a sturday." + "" + "Your akan name is" + maleNames[6];
-          break;
-        default:
-        //console.console.log("pass");//Test male case
+
+lyear = true;
+
+}
+
+if ((lyear==false) && (date>=29))
+
+{
+
+alert('Invalid date format!');
+
+return false;
+
+}
+
+if ((lyear==true) && (date>29))
+
+{
+
+alert('Invalid date format!');
+
+return false;
+
+}
+
+}
+
+if(isNaN(year) || year === "" ||year === null || year <1900 || year > currentYear){
+
+document.getElementById('three').innerHTML = "invalid year";
+
+document.getElementById('three').style.color = "red"
+
+  alert("invalid year");
+
+  return false;
+
+}
+
+if (gen[0].checked === false && gen[1].checked === false) {
+
+ 
+
+  alert('select gender');
+
+  
+
+} else {
+
+   for (i = 0; i < gen.length; i++) {
+
+       if (gen[i].checked){
+
+          alert(gen[i].value);
+
+              var gend = gen[i].value;
+
+   }
+
+              
+
+              var userInputs ={
+
+                  userDate: date,
+
+                  userMonth: month,
+
+                  userYear: year,
+
+                  userGender: gend
+
+              };
+
+              
+
+              return userInputs;
+
+              }
+
+              
+
+          
+
+          }           
+
+}
+
+function myFunction(){
+
+var a = validation();
+
+var DD = parseInt(a.userDate);
+
+var MM = parseInt(a.userMonth);
+
+var newYear = a.userYear;
+
+var YY = parseInt(newYear.slice(2,4));
+
+var CC = parseInt(newYear.slice(0,2));
+
+var newGender = a.userGender;
+
+
+var a = Math.floor((14 - MM) / 12);
+
+var y = newYear - a;
+
+var m = MM + 12 * a - 2;
+
+var dayOfTheWeek = (DD + y + Math.floor(y / 4) - Math.floor(y / 100) +
+
+Math.floor(newYear / 400) + Math.floor((31 * m) / 12)) % 7;
+
+
+
+var daysOfaweek = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday',       
+
+   'Thursday', 'Friday', 'Saturday');
+
+
+
+var realDay = daysOfaweek[dayOfTheWeek];
+
+if (newGender ==="male") {
+
+for (var name in maleNames) {
+
+  if (maleNames.hasOwnProperty(name)) {
+
+      if (name === realDay) {
+
+          
+
+          alert("Your Akan name is "+maleNames[name]);
+
       }
-      break;
-    case gender = "female":
-      switch (day) {
-        case 0 || -0:
-          document.getElementById("result").innerHTML = "The day is on a sunday." + "" + "Your akan name is akosua";
-          break;
-        case 1 || -1:
-          document.getElementById("result").innerHTML = "The day is on a monday." + "" + "Your akan name is adwoa";
-          break;
-        case 2 || -2:
-          document.getElementById("result").innerHTML = "The day is on a tuesday." + "" + "Your akan name is abenaa";
-          break;
-        case 3 || -3:
-          document.getElementById("result").innerHTML = "The day is on a wednesday." + "" + "Your akan name is akua";
-          break;
-        case 4 || -4:
-          document.getElementById("result").innerHTML = "The day is on a thursaday." + "" + "your akan name is yaa";
-          break;
-        case 5 || -5:
-          document.getElementById("result").innerHTML = "The day is on a friday." + "" + "Your akan name is afua";
-          break;
-        case 6 || -6:
-          document.getElementBtId("result").innerHTML = "The day is on a saturday." + "" + "Your akan name is ama";
-          break
+
+  }
+
+}
+
+}else {
+
+for (var name in femaleNames) {
+
+      if (femaleNames.hasOwnProperty(name)) {
+
+          if (name === realDay) {
+
+              
+
+              alert("Your Akan name is "+femaleNames[name]);
+
+          }
 
       }
-      break
-    default:
-      console.log("pass");//Test gender switch
+
   }
+
+
+
 }
+
+}
+
