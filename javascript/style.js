9b81114012af55b9f156da2c08a7c68f4cf027df
Alex Kiprop <kipropalex59@gmail.com>
2019-08-24 10:40:42 +0300

    changed the check day of week from invalid to check of week so as to work as it is expected

--- javascript/style.js
+++ javascript/style.js
@@ -2,35 +2,35 @@ var maleNames = {
 
   Sunday: 'Kwasi',
 
-Monday: 'Kwadwo',
+  Monday: 'Kwadwo',
 
-Tuesday: 'Kwabena',
+  Tuesday: 'Kwabena',
 
-Wednesday: 'Kwaku',
+  Wednesday: 'Kwaku',
 
-Thursday: 'Yaw',
+  Thursday: 'Yaw',
 
-Friday: 'Kofi',
+  Friday: 'Kofi',
 
-Saturday: 'Kwame',
+  Saturday: 'Kwame',
 
 };
 
 var femaleNames = {
 
-Sunday: 'Akosua',
+  Sunday: 'Akosua',
 
-Monday: 'Adwoa',
+  Monday: 'Adwoa',
 
-Tuesday: 'Abenaa',
+  Tuesday: 'Abenaa',
 
-Wednesday: 'Akua',
+  Wednesday: 'Akua',
 
-Thursday: 'Yaa',
+  Thursday: 'Yaa',
 
-Friday: 'Afua',
+  Friday: 'Afua',
 
-Saturday: 'Ama',
+  Saturday: 'Ama',
 
 };
 
@@ -38,234 +38,213 @@ Saturday: 'Ama',
 
 function validation() {
 
-var date = document.getElementById("d").value;
+  var date = document.getElementById("d").value;
 
 
 
-var month = document.getElementById("month").value;
+  var month = document.getElementById("month").value;
 
-var year = document.getElementById("year").value;
+  var year = document.getElementById("year").value;
 
-var gen = document.getElementsByName("gd");
+  var gen = document.getElementsByName("gd");
 
-var currentDate = new Date();
+  var currentDate = new Date();
 
-var currentYear = currentDate.getFullYear();
+  var currentYear = currentDate.getFullYear();
 
-if(isNaN(date) || date === null || date === "" || date < 1 || date > 31){
+  if (isNaN(date) || date === null || date === "" || date < 1 || date > 31) {
 
- 
+    alert("invalid date");
 
-  document.getElementById('one').innerHTML = "invalid day";
+    return false;
 
-  document.getElementById('one').style.color = "red"
+  }
 
-  alert("invalid date");
+  var ListofDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-  return false;
-
-}
-
-var ListofDays = [31,28,31,30,31,30,31,31,30,31,30,31];
-
-if(isNaN(month) || month=== "" || month ===null || month <1 || month>12){
-
-  document.getElementById('two').innerHTML = "invalid month";
-
-  document.getElementById('two').style.color = "red"
-
-  alert("invalid month");
-
- return false;
-
-}
-
-if(month===1 || month>2){
-
-if (date>ListofDays[month-1])
-
-{
-
-alert('Invalid date format!');
-
-return false;
-
-}
-
-}
-
-if(month===2){
-
-var lyear = false;
-
-if ( (!(year % 4) && year % 100) || !(year % 400)) 
-
-{
-
-
-
-lyear = true;
-
-}
-
-if ((lyear==false) && (date>=29))
-
-{
-
-alert('Invalid date format!');
-
-return false;
-
-}
-
-if ((lyear==true) && (date>29))
-
-{
-
-alert('Invalid date format!');
-
-return false;
-
-}
-
-}
-
-if(isNaN(year) || year === "" ||year === null || year <1900 || year > currentYear){
-
-document.getElementById('three').innerHTML = "invalid year";
-
-document.getElementById('three').style.color = "red"
-
-  alert("invalid year");
-
-  return false;
-
-}
-
-if (gen[0].checked === false && gen[1].checked === false) {
+  if (isNaN(month) || month === "" || month === null || month < 1 || month > 12) {
 
  
 
-  alert('select gender');
+    alert("invalid month");
+
+    return false;
+
+  }
+
+  if (month === 1 || month > 2) {
+
+    if (date > ListofDays[month - 1]) {
+
+      alert('Invalid date format!');
+
+      return false;
+
+    }
+
+  }
+
+  if (month === 2) {
+
+    var lyear = false;
+
+    if ((!(year % 4) && year % 100) || !(year % 400)) {
+
+
+
+      lyear = true;
+
+    }
+
+    if ((lyear == false) && (date >= 29)) {
+
+      alert('Invalid date format!');
+
+      return false;
+
+    }
+
+    if ((lyear == true) && (date > 29)) {
+
+      alert('Invalid date format!');
+
+      return false;
+
+    }
+
+  }
+
+  if (isNaN(year) || year === "" || year === null || year < 1900 || year > currentYear) {
+
+
+    alert("invalid year");
+      
+    return false;
+
+  }
+
+  if (gen[0].checked === false && gen[1].checked === false) {
+
+
+
+    alert('select gender');
+
+
+
+  } else {
+
+    for (var i = 0; i < gen.length; i++) {
+
+      if (gen[i].checked) {
+
+        alert(gen[i].value);
+
+        gend = gen[i].value;
+
+      }
+
+
+    }
+      var userInputs = {
+
+        userDate: date,
+
+        userMonth: month,
+
+        userYear: year,
+
+        userGender: gend
+
+      };
+
+
+
+      return userInputs;
+
+    
+
+
+
+
+
+  }
+
+}
+
+function myFunction() {
+
+  var a = validation();
+
+  var DD = parseInt(a.userDate);
+
+  var MM = parseInt(a.userMonth);
+
+  var newYear = a.userYear;
 
   
 
-} else {
-
-   for (i = 0; i < gen.length; i++) {
-
-       if (gen[i].checked){
-
-          alert(gen[i].value);
-
-              var gend = gen[i].value;
-
-   }
-
-              
-
-              var userInputs ={
-
-                  userDate: date,
-
-                  userMonth: month,
-
-                  userYear: year,
-
-                  userGender: gend
-
-              };
-
-              
-
-              return userInputs;
-
-              }
-
-              
-
-          
-
-          }           
-
-}
-
-function myFunction(){
-
-var a = validation();
-
-var DD = parseInt(a.userDate);
-
-var MM = parseInt(a.userMonth);
-
-var newYear = a.userYear;
-
-var YY = parseInt(newYear.slice(2,4));
-
-var CC = parseInt(newYear.slice(0,2));
-
-var newGender = a.userGender;
+  var newGender = a.userGender;
 
 
-var a = Math.floor((14 - MM) / 12);
+  var a = Math.floor((14 - MM) / 12);
 
-var y = newYear - a;
+  var y = newYear - a;
 
-var m = MM + 12 * a - 2;
+  var m = MM + 12 * a - 2;
 
-var dayOfTheWeek = (DD + y + Math.floor(y / 4) - Math.floor(y / 100) +
+  var dayOfTheWeek = (DD + y + Math.floor(y / 4) - Math.floor(y / 100) +
 
-Math.floor(newYear / 400) + Math.floor((31 * m) / 12)) % 7;
+    Math.floor(newYear / 400) + Math.floor((31 * m) / 12)) % 7;
 
 
 
-var daysOfaweek = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday',       
+  var daysOfaweek = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday',
 
-   'Thursday', 'Friday', 'Saturday');
+    'Thursday', 'Friday', 'Saturday');
 
 
 
-var realDay = daysOfaweek[dayOfTheWeek];
+  var realDay = daysOfaweek[dayOfTheWeek];
 
-if (newGender ==="male") {
+  if (newGender === "male") {
 
-for (var name in maleNames) {
+    for (var name in maleNames) {
 
-  if (maleNames.hasOwnProperty(name)) {
+      if (maleNames.hasOwnProperty(name)) {
 
-      if (name === realDay) {
+        if (name === realDay) {
 
-          
 
-          alert("Your Akan name is "+maleNames[name]);
+
+          alert("Your Akan name is " + maleNames[name]);
+
+        }
 
       }
 
-  }
+    }
 
-}
+  } else {
 
-}else {
-
-for (var name in femaleNames) {
+    for (var name in femaleNames) {
 
       if (femaleNames.hasOwnProperty(name)) {
 
-          if (name === realDay) {
+        if (name === realDay) {
 
-              
 
-              alert("Your Akan name is "+femaleNames[name]);
 
-          }
+          alert("Your Akan name is " + femaleNames[name]);
+
+        }
 
       }
 
+    }
+
+
+
   }
-
-
-
-}
 
 }
 
